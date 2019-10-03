f9bb2c5e3a773f5ea4f936e1be9390d5e2d3b275
Jonas Hellstrom <51347931+pjhellstrom@users.noreply.github.com>
2019-10-03 15:34:36 -0400

    Delete config.js

--- huddletime-functions/functions/utils/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyClCMBwZNDgMGR4_l6RuPQaynlHgFhy89Q",
-  authDomain: "huddletime.firebaseapp.com",
-  databaseURL: "https://huddletime.firebaseio.com",
-  projectId: "huddletime",
-  storageBucket: "huddletime.appspot.com",
-  messagingSenderId: "763726161454",
-  appId: "1:763726161454:web:e9158bf32633227f3e21bf"
-};
